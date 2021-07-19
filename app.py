1877cfc6a5694094ae8f2b923ad4f80c72ffe0a3
Juyoung Chung <84014277+dilatata@users.noreply.github.com>
2021-07-19 17:19:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def get():
 def shop():
     return render_template('shop.html')  
 
+@app.route('/join', methods=['get', 'post'])
+def join():
+    return render_template('join.html')   
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port="5000")
